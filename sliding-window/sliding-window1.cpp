4a1e19e0bbaed7cd6ed92fec3e6e30068bbb497d
adityas-opse <85329735+adityas-ops@users.noreply.github.com>
2023-02-05 01:14:25 +0530

    first

--- sliding-window/sliding-window1.cpp
+++ sliding-window/sliding-window1.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 using namespace std;
 // this is first question about sliding windows
-// print all subarray of size k
-void printSubArray(int arr[], int n)
+// question : find the maximum sum of given size in the given array
+// here array and size is given
+
+int findMax(int arr[], int n, int k)
 {
-    for (int i = 0; i < n; i++)
+    int max_sum = 0;
+    // here firstly we calcualte first k element sum
+    for (int i = 0; i < k; i++)
     {
-        for (int j = i; j < n; j++)
-        {
-            for (int k = i; k <= j; k++)
-            {
-                cout << arr[k] << " ";
-            }
-            cout << endl;
-        }
+        max_sum += arr[i];
     }
+    int window_sum = max_sum;
+    //  now after initialising the window sum as max sum
+    // we will start the loop from k to n
+    // and we will subtract the first element of the window and add the next element of the window
+    // and we will compare the window sum with the max sum
+    // and we will update the max sum if the window sum is greater than the max sum
+    for (int i = k; i < n; i++)
+    {
+        window_sum += arr[i] - arr[i - k];
+        max_sum = max(max_sum, window_sum);
+    }
+    return max_sum;
 }
 
 int main()
@@ -22,7 +31,7 @@ int main()
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
     int n = sizeof(arr) / sizeof(arr[0]);
     int k = 3;
-    printSubArray(arr, n);
+    cout << findMax(arr, n, k);
 
     return 0;
 }
