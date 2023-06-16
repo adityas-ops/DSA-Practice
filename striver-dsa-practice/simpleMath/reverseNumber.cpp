3eb2faf6869f996f9889584d147cd27622369c2a
adityas-opse <85329735+adityas-ops@users.noreply.github.com>
2023-06-16 22:22:44 +0530

    math

---
+++ striver-dsa-practice/simpleMath/reverseNumber.cpp
@@ -0,0 +1,26 @@
+/*
+
+intput 123
+output 321
+
+*/
+#include<iostream>
+using namespace std;
+
+ int reverseInt(int n){
+      long r=0;      // decleare r 
+        while(x){
+         r=r*10+x%10; // find remainder and add its to r
+         x=x/10;     // Update the value of x
+        }
+        if(r>INT_MAX || r<INT_MIN) return 0; // check range if r is outside the range then return 0  
+        return int(r);  // if r in the 32 bit range then return r
+ }
+
+int main(){
+    int n;
+    cin>>n;
+    // reverse the integer
+   cout<<reverseInt(n)<<endl;
+        return 0;
+}
